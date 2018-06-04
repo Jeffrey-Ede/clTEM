1898ee80cf0c92445c88aa0dc2c27f9d64f60fd1
JJPPeters <j.peters.1@warwick.ac.uk>
2018-06-04 16:04:32 +0100

    Attempted again to fix hover issue

--- js/navbar.js
+++ js/navbar.js
@@ -11,3 +11,38 @@ function toggleDropdown() {
     document.getElementsByClassName("dropdown-content")[0].classList.toggle("displayed");
     document.getElementsByClassName("dropdown")[0].classList.toggle("expanded");
 }
+
+// https://stackoverflow.com/questions/23885255/how-to-remove-ignore-hover-css-style-on-touch-devices
+function watchForHover() {
+    var hasHoverClass = false;
+    var container = document.body;
+    var lastTouchTime = 0;
+
+    function enableHover() {
+        // filter emulated events coming from touch events
+        if (new Date() - lastTouchTime < 500) return;
+        if (hasHoverClass) return;
+
+        container.className += ' hasHover';
+        hasHoverClass = true;
+    }
+
+    function disableHover() {
+        if (!hasHoverClass) return;
+
+        container.className = container.className.replace(' hasHover', '');
+        hasHoverClass = false;
+    }
+
+    function updateLastTouchTime() {
+        lastTouchTime = new Date();
+    }
+
+    document.addEventListener('touchstart', updateLastTouchTime, true);
+    document.addEventListener('touchstart', disableHover, true);
+    document.addEventListener('mousemove', enableHover, true);
+
+    enableHover();
+}
+
+// watchForHover();

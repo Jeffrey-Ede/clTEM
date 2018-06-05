9d8c8cebab633a82a6b03a7360220c80ee1b7f3f
JJPPeters <j.peters.1@warwick.ac.uk>
2018-06-05 21:25:47 +0100

    Fixed some javascript

--- js/navbar.js
+++ js/navbar.js
@@ -12,16 +12,6 @@ function toggleDropdown() {
     document.getElementsByClassName("dropdown")[0].classList.toggle("expanded");
 }
 
-// https://stackoverflow.com/questions/11309859/css-media-queries-and-javascript-window-width-do-not-match
-function viewport() {
-    var e = window, a = 'inner';
-    if (!('innerWidth' in window )) {
-        a = 'client';
-        e = document.documentElement || document.body;
-    }
-    return { width : e[ a+'Width' ] , height : e[ a+'Height' ] };
-}
-
 // https://stackoverflow.com/questions/23885255/how-to-remove-ignore-hover-css-style-on-touch-devices
 function watchForHover() {
     var hasHoverClass = false;
@@ -48,42 +38,9 @@ function watchForHover() {
         lastTouchTime = new Date();
     }
 
-
-    var lastSize = 0;
-    function updateResize() {
-        var w = viewport(); 
-        if (w.width > 680) {
-            if (lastSize <= 680) {
-                document.getElementsByClassName("dropdown-content")[0].classList.toggle("displayed", false);
-                document.getElementsByClassName("dropdown")[0].classList.toggle("expanded", false);
-                closeMenu();
-            }
-
-            var ac = document.getElementsByClassName('dropbtn')
-            for (var i = 0; i < ac.length; i++) {
-                if (ac[i].href_backup != null) {
-                ac[i].href = ac[i].href_backup;
-                ac[i].onlick_backup = null;
-                }
-            }
-
-        } else {
-            var ac = document.getElementsByClassName('dropbtn')
-            for (var i = 0; i < ac.length; i++) {
-                if (ac[i].href != "javascript:void(0)") {
-                ac[i].href_backup = ac[i].href;
-                ac[i].href = "javascript:void(0)";//function() {return false;};
-            }
-            }
-        }
-
-        lastSize = w.width;
-    }
-
     document.addEventListener('touchstart', updateLastTouchTime, true);
     document.addEventListener('touchstart', disableHover, true);
     document.addEventListener('mousemove', enableHover, true);
-    window.addEventListener("resize", updateResize, false);
 
     enableHover();
 }
@@ -100,4 +57,46 @@ function colorTaps() {
       }
     }
 }
-// watchForHover();
+
+
+function watchForResize() {
+
+    // https://stackoverflow.com/questions/11309859/css-media-queries-and-javascript-window-width-do-not-match
+    function viewport() {
+        var e = window, a = 'inner';
+        if (!('innerWidth' in window )) {
+            a = 'client';
+            e = document.documentElement || document.body;
+        }
+        return { width : e[ a+'Width' ] , height : e[ a+'Height' ] };
+    }
+
+    var lastSize = 0;
+    function updateResize() {
+        var w = viewport(); 
+        if (w.width > 680) {
+            if (lastSize <= 680) {
+                document.getElementsByClassName("dropdown-content")[0].classList.toggle("displayed", false);
+                document.getElementsByClassName("dropdown")[0].classList.toggle("expanded", false);
+                closeMenu();
+            }
+
+            var ac = document.getElementsByClassName('dropbtn')
+            for (var i = 0; i < ac.length; i++) {
+                ac[i].onclick = function(){}
+            }
+
+        } else {
+            var ac = document.getElementsByClassName('dropbtn')
+            for (var i = 0; i < ac.length; i++) {
+            ac[i].onclick = function(){return false;}
+            }
+        }
+
+        lastSize = w.width;
+    }
+
+    window.addEventListener("resize", updateResize, false);
+
+    updateResize();
+}
